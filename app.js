b79a65ca2b62d20ccf516fd70079696000648f18
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-10-05 22:53:23 +0300

    104

--- app.js
+++ app.js
@@ -103,6 +103,42 @@ app.get('/books/:categ_id', (req, res) => {
     }
 });
 
+app.get('/person/:person_id/books/:book_id', (req, res) => {
+    const book_id = 'book_id';
+    const person_id = 'person_id';
+    const body = 'body';
+    const p_quantity = 'p_quantity';
+    const p_book = 'p_book';
+
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection(connJson);
+
+    if (req && body in req && req.params && book_id in req.params && person_id in req.params) {
+        var b_id = req.params[book_id];
+        var p_id = req.params[person_id];
+
+        if (b_id && p_id) {
+            con.connect((err) => {
+                if (err) throw err;
+
+                sql = "select *, person_book.quantity as p_quantity from person_book inner join book on book.Id = person_book.book_id where person_id = " + p_id.toString()
+                sql += " and book_id = " + b_id.toString();
+
+                console.log(sql);
+
+                con.query(sql, function(err, results, fields) {
+                    if (err) throw err;
+
+                    res.send(results);
+                });
+
+                con.end();
+            });
+        }
+    }
+});
+
 app.post('/person/:person_id/books/:book_id', (req, res) => {
     const book_id = 'book_id';
     const person_id = 'person_id';
