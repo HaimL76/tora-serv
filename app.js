7d67155e6d44841889c4ed2d296ddd90ca993c23
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2020-10-29 00:15:28 +0200

    148

--- app.js
+++ app.js
@@ -123,16 +123,17 @@ app.get('/person/:person_id/books/:book_id', (req, res) => {
             con.connect((err) => {
                 if (err) throw err;
 
-                sql = "select *, person_book.id as p_b_id, person_book.quantity as p_quantity, person_book.progress_counter as progress_counter, "
+                sql = "select *, book_category.name as categ_name, person_book.id as p_b_id, person_book.quantity as p_quantity, person_book.progress_counter as progress_counter, "
                 sql += " achievements0.max_number "
                     //aql += " max(achievments.number) as max_achievement "
                 sql += " from person_book ";
-                sql += " inner join book on book.Id = person_book.book_id, "
-                    //sql += " left outer join achievements on achievements on achievements.person_book = person_book.id "
+                sql += " inner join book on book.id = person_book.book_id ";
+                sql += " inner join book_category on book.category = book_category.id, ";
+                //sql += " left outer join achievements on achievements on achievements.person_book = person_book.id "
 
                 sql += " lateral (select count(*) as max_number from achievements where person_book = person_book.id) achievements0 "
 
-                sql += " where person_id = " + p_id.toString()
+                sql += " where person_id = " + p_id.toString();
                 sql += " and book_id = " + b_id.toString();
 
                 console.log(sql);
@@ -214,25 +215,29 @@ app.post('/person_book/:p_b_id', (req, res) => {
                                                     sql += " values (" + person_book.toString() + ", '" + achieve_val + "')";
 
                                                     con.query(sql, function(err, results, fields) {
-                                                        if (err) throw err;
+                                                        if (err)
+                                                            res.send(false);
 
                                                         res.send(results);
 
                                                         con.commit((err) => {
 
                                                         });
+
                                                         con.end();
                                                     });
                                                 } else {
                                                     con.commit((err) => {
 
                                                     });
+
                                                     con.end;
                                                 }
                                             } else {
                                                 con.commit((err) => {
 
                                                 });
+
                                                 con.end();
                                             }
                                         });
@@ -240,12 +245,14 @@ app.post('/person_book/:p_b_id', (req, res) => {
                                         con.commit((err) => {
 
                                         });
+
                                         con.end();
                                     }
                                 } else {
                                     con.commit((err) => {
 
                                     });
+
                                     con.end();
                                 }
                             });
@@ -306,10 +313,11 @@ app.get('/person/:id', (req, res) => {
             con.connect((err) => {
                 if (err) throw err;
 
-                sql = "select *, person_book.id as p_b_id, person_book.quantity as p_quantity, achievements0.max_number "
+                sql = "select *, book_category.name as categ_name, person_book.id as p_b_id, person_book.quantity as p_quantity, achievements0.max_number "
                 sql += " from person "
                 sql += " left outer join person_book on person.id = person_book.person_id ";
-                sql += " inner join book on book.id = person_book.book_id, ";
+                sql += " inner join book on book.id = person_book.book_id ";
+                sql += " inner join book_category on book.category = book_category.id, ";
                 //sql += " left outer join achievements on achievements.person_book = person_book.id "
 
                 sql += " lateral (select count(*) as max_number from achievements where person_book = person_book.id) achievements0 "
